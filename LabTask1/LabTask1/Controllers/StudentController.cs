9a3e39444f99ccf03f4f7701d97f637a6222aa2e
78ahmed <inunnaher7778@gmail.com>
2021-06-16 11:02:34 +0600

    This is my first .Net lab Task

---
+++ LabTask1/LabTask1/Controllers/StudentController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabTask1.Models;
+using LabTask1.Models.Database;
+
+namespace LabTask1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult Index()
+        {
+            Database db = new Database();
+            var students=db.Students.GetAll();
+            return View(students);
+        }
+        public ActionResult Login()
+        {
+            Admin a = new Admin();
+            return View(a);
+        }
+        [HttpPost]
+        public ActionResult Login(Admin a)
+        {
+            //return View(a);
+            return RedirectToAction("Dashboard");
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult CreateS()
+        {
+            Student s = new Student();
+            return View(s);
+        }
+        [HttpPost]
+        public ActionResult CreateS(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                Database db = new Database();
+                db.Students.Insert(s);
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public ActionResult Edit(int id)
+        {
+            Database db = new Database();
+            var s = db.Students.Get(id);
+            return View(s);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student s)
+        {   
+            Database db = new Database();
+            db.Students.Update(s);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            Database db = new Database();
+            db.Students.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ LabTask1/LabTask1/Models/Database/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace LabTask1.Models.Database
+{
+    public class Database
+    {
+        public Admins Admins { get; set; }
+        public Students Students { get; set; }
+        public Depertments Depertments { get; set; }
+        public Database()
+        {
+            string connString = @"Server=DESKTOP-DVV1NA8\SQLEXPRESS;Database=LabTask1;Integrated Security=true;";
+            SqlConnection conn = new SqlConnection(connString);
+
+            Admins = new Admins(conn);
+            Students = new Students(conn);
+            Depertments = new Depertments(conn);
+        }
+    }
+}
